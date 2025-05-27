31165c14934ef9f88d3c98b909beb8e99cac0469
Winlypse <nts3@o2.pl>
2025-05-27 23:21:25 +0200

    Initial commit

---
+++ src/main/kotlin/winlyps/openDoors/OpenDoors.kt
@@ -0,0 +1,14 @@
+package winlyps.openDoors
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class OpenDoors : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
